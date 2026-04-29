57726c4af0c74a84d582e9ce8db7ea9c8b34c103
Asmit Chhotaray <asmitchhotaray@gmail.com>
2026-04-29 22:28:37 +0530

    Create the Header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import { Plus } from "lucide-react";
+export function Header() {
+  return (
+    <div className="flex items-center justify-between">
+      <h1 className="text-3xl font-bold">Dashboard</h1>
+      <button className="bg-[#4B5CEE]  text-gray-100 px-6 py-4 rounded-[10px] cursor-pointer">
+        <div className="flex gap-2">
+          <Plus className="size-6" strokeWidth={2} />
+          <p className="text-[17px]">Add Transaction</p>
+        </div>
+      </button>
+    </div>
+  );
+}
--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -1,10 +1,13 @@
 import { Sidebar } from "../components/Sidebar";
+import { Header } from "../components/Header";
 
 export function DashBoard(){
   return (
-    <div className="app-layout">
+    <div className="app-layout flex min-h-screen ">
       <Sidebar />
-
+      <main className="flex-1 bg-gray-100 py-8 px-10">
+        <Header/>
+      </main>
      
     </div>
   );
