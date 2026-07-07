9e3db7dc086776da4250c21bcedf6dfcee7c4dfa
Asmit Chhotaray <asmitchhotaray@gmail.com>
2026-07-07 13:13:25 +0530

    add Income vs Expense bar chart component to visualize financial data

--- frontend/src/components/charts/Charts.jsx
+++ frontend/src/components/charts/Charts.jsx
@@ -1,9 +1,10 @@
 import { ExpensePieChart } from "./ExpensePieChart"
+import { IncomeExpenseBarChart } from "./IncomeExpenseBarChart"
 export function Charts({transactions}) {
   return (
-    <div className="flex  justify-between">
+    <div className="flex w-full h-[350px] justify-between">
      <ExpensePieChart transactions={transactions} />
-   
+      <IncomeExpenseBarChart transactions={transactions} />
     </div>
   )
 }
---
+++ frontend/src/components/charts/IncomeExpenseBarChart.jsx
@@ -0,0 +1,66 @@
+import {
+  ResponsiveContainer,
+  Bar,
+  BarChart,
+  Legend,
+  Tooltip,
+  YAxis,
+  XAxis,
+  CartesianGrid
+} from "recharts";
+
+export function IncomeExpenseBarChart({ transactions }) {
+  const chartData = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "income") {
+        acc.income += transaction.amount;
+      } else {
+        acc.expense += transaction.amount;
+      }
+      return acc;
+    },
+    { income: 0, expense: 0 }
+  );
+
+  const data = [
+    {
+      name: "Income Vs Expense",
+      income: chartData.income,
+      expense: chartData.expense,
+    },
+  ];
+
+  if(chartData.income === 0 && chartData.expense === 0) {
+    return (
+      <div className="w-full h-[350px] flex items-center justify-center">
+        <p className="dark:text-white">No Income Vs Expense <br /> data available</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="w-full h-[350px]">
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart data={data} barCategoryGap="40%">
+          <CartesianGrid strokeDasharray="3 3" stroke="#374151" />
+          <XAxis dataKey="name" stroke="#9CA3AF" />
+          <YAxis stroke="#9CA3AF" />
+          <Tooltip />
+          <Legend />
+          <Bar
+            dataKey="income"
+            fill="#22C55E"
+            radius={[8, 8, 0, 0]}
+            label={{ position: "top" }}
+          />
+          <Bar
+            dataKey="expense"
+            fill="#EF4444"
+            radius={[8, 8, 0, 0]}
+            label={{ position: "top" }}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
